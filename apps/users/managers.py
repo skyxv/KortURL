de53628e235fc507aada75c9c3b4101b78e426b8
yandenghong <vincentyim8@163.com>
2019-03-25 16:49:03 +0800

    Updated UserManager

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -8,31 +8,21 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    use_in_migrations = True
 
-    def _create_user(self, username, password, **extra_fields):
-        """
-        Create and save a user with the given username and password.
-        """
+    def create_user(self, username, password=None):
         if not username:
             raise ValueError('The given username must be set')
         username = self.model.normalize_username(username)
-        user = self.model(username=username, **extra_fields)
+        user = self.model(username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, password, **extra_fields)
-
-    def create_superuser(self, username, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(username, password, **extra_fields)
+    def create_superuser(self, username, password):
+        user = self.create_user(
+            username,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
