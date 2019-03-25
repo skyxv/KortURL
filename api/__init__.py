9e3e7d9445f136f0d1b38810395a13200b8e7a6f
yandenghong <vincentyim8@163.com>
2019-03-25 11:11:43 +0800

    Initial commit

---
+++ api/__init__.py
@@ -0,0 +1,8 @@
+"""
+*  Time:     19-3-25 上午11:08
+*  Author:   Vincent Yim
+*  FileName: __init__.py.py
+*  Software: PyCharm
+*  Blog:     https://yandenghong.github.io
+"""
+
---
+++ apps/__init__.py
@@ -0,0 +1,8 @@
+"""
+*  Time:     19-3-25 上午11:07
+*  Author:   Vincent Yim
+*  FileName: __init__.py.py
+*  Software: PyCharm
+*  Blog:     https://yandenghong.github.io
+"""
+
