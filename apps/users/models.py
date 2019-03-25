030bbd18be7e215b01f46f94bb9c294469b9ccf9
yandenghong <vincentyim8@163.com>
2019-03-25 17:16:09 +0800

    Added create_auth_token method to generating a token when create a user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 from .managers import UserManager
 
@@ -31,3 +35,9 @@ class User(AbstractBaseUser):
         db_table = "user"
         verbose_name = "用户"
         verbose_name_plural = verbose_name
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
