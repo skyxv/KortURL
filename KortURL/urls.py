b75e70b5befee2f964158e365dc80db93c720083
yandenghong <vincentyim8@163.com>
2019-03-25 17:27:18 +0800

    inclued api urls

--- KortURL/urls.py
+++ KortURL/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from api import urls as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(api_urls, namespace='api')),
+
 ]
