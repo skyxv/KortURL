079cc7005f878af36dd75aa6834bc736a168e220
yandenghong <vincentyim8@163.com>
2019-03-25 17:31:24 +0800

    Added app_name

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 
 from api import views
 
+app_name = "api"
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
