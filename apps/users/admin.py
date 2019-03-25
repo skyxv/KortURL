9e07fe9966f951861eb5dfc61bd96e16548c8ea5
yandenghong <vincentyim8@163.com>
2019-03-25 16:58:49 +0800

    Added UserAdmin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from . import forms, models
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+
+    form = forms.UserChangeForm
+    add_form = forms.UserCreationForm
+
+    list_display = ('username', 'email', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('id',)
+    filter_horizontal = ()
