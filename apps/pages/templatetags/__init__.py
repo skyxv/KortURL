d4258c56220e677fef8c6f160ad24029b404e65f
yandenghong <vincentyim8@163.com>
2019-03-26 11:03:38 +0800

    Added get_site_name templatetags

---
+++ apps/pages/templatetags/__init__.py
@@ -0,0 +1,8 @@
+"""
+*  Time:     19-3-26 上午10:50
+*  Author:   Vincent Yim
+*  FileName: __init__.py.py
+*  Software: PyCharm
+*  Blog:     https://yandenghong.github.io
+"""
+
---
+++ apps/pages/templatetags/custom_tags.py
@@ -0,0 +1,16 @@
+"""
+*  Author:   Vincent Yim
+*  FileName: custom_tags.py
+*  Software: PyCharm
+*  Blog:     https://yandenghong.github.io
+"""
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter
+def get_site_name(value):
+    return settings.KORT_URL.get('SITE_NAME', value)
+
