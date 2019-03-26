676a55a3fa30d8a6a977cbf8614c7c0eb500694f
yandenghong <vincentyim8@163.com>
2019-03-26 16:45:58 +0800

    Created Login ,Logout views

--- KortURL/urls.py
+++ KortURL/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from api import urls as api_urls
+from apps.users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urls, namespace='api')),
+    path('login/', user_views.Login.as_view(), name="login"),
+    path('logout/', user_views.Logout.as_view(), name="logout"),
 
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views import View
 
-# Create your views here.
+from .forms import LoginForm
+
+
+class Login(View):
+    """
+    登录
+    """
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request, 'login.html', {"error": "用户名或密码错误"})
+        else:
+            return render(request, 'login.html', {"error": form.errors})
+
+
+class Logout(View):
+
+    @method_decorator(login_required)
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse("logout"))
