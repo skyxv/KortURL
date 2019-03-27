ff4725fb0212fb865e0c2632a7b5a09bb7106568
yandenghong <vincentyim8@163.com>
2019-03-27 17:00:14 +0800

    Added created_by field

--- apps/urls/models.py
+++ apps/urls/models.py
@@ -15,6 +15,7 @@ class LinkMap(models.Model):
     hit_count = models.IntegerField("打开次数", default=0)
     init_access_at = models.DateTimeField("初次访问时间", null=True, blank=True)
     expire_time = models.IntegerField("过期时间(单位：秒)", default=0)
+    created_by = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name="创建人")
     created_at = models.DateTimeField("创建时间", auto_now_add=True)
     updated_at = models.DateTimeField("更新时间", auto_now=True)
 
