416216b58c27ea4826c5d0e207a9e5d3d8563461
yandenghong <vincentyim8@163.com>
2019-03-29 17:26:38 +0800

    Created Doc View

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Doc(TemplateView):
+    """
+    接口文档
+    """
+    template_name = 'doc.html'
