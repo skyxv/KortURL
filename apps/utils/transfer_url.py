5b4f07ea2406527f09930c286554514d15a82058
yandenghong <vincentyim8@163.com>
2019-04-01 15:02:25 +0800

    Code structure optimization

--- apps/utils/transfer_url.py
+++ apps/utils/transfer_url.py
@@ -4,8 +4,9 @@
 *  Software: PyCharm
 *  Blog:     https://yandenghong.github.io
 """
-from django.conf import settings
 from django.utils.crypto import get_random_string
+
+from apps.utils.get_configs import config
 
 
 class CodeGenerator:
@@ -13,9 +14,8 @@ class CodeGenerator:
     生成短码
     """
     def __init__(self):
-        self.code_max_length = settings.KORT_URL.get("CODE_MAX_LENGTH", 7)
-        self.code_allow_chars = settings.KORT_URL.get("CODE_ALLOWED_CHARS",
-                                                      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+        self.code_max_length = config.code_length
+        self.code_allow_chars = config.code_allowed_chars
 
     def get_code(self):
         return get_random_string(length=self.code_max_length, allowed_chars=self.code_allow_chars)
