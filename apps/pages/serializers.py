72af8abf3a18b719b8769b73115882a79221e534
yandenghong <vincentyim8@163.com>
2019-04-01 15:00:45 +0800

    Code structure optimization

--- apps/pages/serializers.py
+++ apps/pages/serializers.py
@@ -4,10 +4,10 @@
 *  Software: PyCharm
 *  Blog:     https://yandenghong.github.io
 """
-from django.conf import settings
 from rest_framework import serializers
 
 from apps.urls.models import LinkMap
+from apps.utils.get_configs import config
 
 
 class LinkMapSerializer(serializers.ModelSerializer):
@@ -20,29 +20,6 @@ class LinkMapSerializer(serializers.ModelSerializer):
     init_time = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
 
-    @property
-    def server_name(self):
-        return settings.KORT_URL.get('SERVER_NAME', 'localhost:8000')
-
-    @property
-    def code_length(self):
-        return settings.KORT_URL.get('CODE_MAX_LENGTH', 7)
-
-    @property
-    def domain(self):
-        protocol = settings.KORT_URL.get('PROTOCOL', 'HTTPS')
-        server_name = self.server_name
-        if protocol.lower() in ["https", "http"]:
-            return protocol.lower() + "://" + server_name
-        else:
-            raise ValueError("Incorrect value of 'KORT_URL.PROTOCOL'.")
-
-    def get_short_url_(self, code):
-        if self.server_name.endswith('/'):
-            return self.domain + code
-        else:
-            return self.domain + "/" + code
-
     class Meta:
         model = LinkMap
         fields = ('long_url', 'short_url', 'counts', 'init_time', 'created_at')
@@ -51,7 +28,7 @@ class LinkMapSerializer(serializers.ModelSerializer):
         return obj.url
 
     def get_short_url(self, obj):
-        return self.get_short_url_(obj.code)
+        return config.get_short_url_(obj.code)
 
     def get_counts(self, obj):
         return obj.hit_count
