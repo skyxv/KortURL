3e2db18a297843974c663ea7cbeb50eac731a531
yandenghong <vincentyim8@163.com>
2019-04-01 16:02:13 +0800

    Updated field acquisition method

--- apps/pages/serializers.py
+++ apps/pages/serializers.py
@@ -7,35 +7,13 @@
 from rest_framework import serializers
 
 from apps.urls.models import LinkMap
-from apps.utils.get_configs import config
 
 
 class LinkMapSerializer(serializers.ModelSerializer):
     """
     history table data serializer
     """
-    long_url = serializers.SerializerMethodField()
-    short_url = serializers.SerializerMethodField()
-    counts = serializers.SerializerMethodField()
-    init_time = serializers.SerializerMethodField()
-    created_at = serializers.SerializerMethodField()
-
     class Meta:
         model = LinkMap
-        fields = ('long_url', 'short_url', 'counts', 'init_time', 'created_at')
-
-    def get_long_url(self, obj):
-        return obj.url
-
-    def get_short_url(self, obj):
-        return config.get_short_url_(obj.code)
-
-    def get_counts(self, obj):
-        return obj.hit_count
-
-    def get_init_time(self, obj):
-        return obj.init_access_at
-
-    def get_created_at(self, obj):
-        return obj.created_at
+        fields = ('url', 'code', 'hit_count', 'init_access_at', 'created_at')
 
