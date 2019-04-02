f5e55677bd585e2db0e441554927c2bbe1781e34
yandenghong <vincentyim8@163.com>
2019-04-02 10:40:11 +0800

    Added get_url_by_code method

--- apps/urls/managers.py
+++ apps/urls/managers.py
@@ -5,7 +5,9 @@
 *  Blog:     https://yandenghong.github.io
 """
 from django.db import models
+
 from apps.utils.transfer_url import code_generator
+from apps.utils.redis.client import redis_cli
 
 
 class LinkMapManager(models.Manager):
@@ -22,6 +24,16 @@ class LinkMapManager(models.Manager):
 
     def get_map_by_code(self, user, code):
         return self.filter(code=code, created_by=user).first()
+
+    def get_url_by_code(self, code):
+        cache_url = redis_cli.get_data(code)
+        if cache_url:
+            return cache_url
+        else:
+            link_map = self.filter(code=code).first()
+            if link_map:
+                redis_cli.set_data(link_map.code, link_map.url)
+                return link_map.url
 
 
 class AccessLogManager(models.Manager):
