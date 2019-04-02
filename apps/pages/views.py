d19bab17d7d9314102f9ea6c8bdfb3a4d777b358
yandenghong <vincentyim8@163.com>
2019-04-02 16:05:29 +0800

    Added view and page graphs of visits, 24-hour access trends, access to devices, operating system statistics.

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -45,6 +45,12 @@ class HistoryDetail(View):
         current_url_logs = AccessLogs.objects.get_log_by_code(link_map.code)
         # 访问量
         view_counts, view_dates = self.get_views_count_data(current_url_logs)
+        # 24小时访问趋势
+        trend_hours, trend_counts = self.get_all_day_visit_trend(current_url_logs)
+        # 设备统计
+        device_names, device_data = self.get_device_data(current_url_logs)
+        # 操作系统统计
+        os_names, os_data = self.get_os_data(current_url_logs)
         return render(request, 'history_detail.html', locals())
 
     def get_views_count_data(self, current_url_logs):
@@ -67,3 +73,46 @@ class HistoryDetail(View):
     def get_str_time(raw_time):
         raw_time = timezone.localtime(raw_time)
         return "{}-{}-{}".format(raw_time.year, raw_time.month, raw_time.day)
+
+    @staticmethod
+    def get_all_day_visit_trend(current_url_logs):
+        """
+        24小时访问趋势
+        """
+        import datetime
+        now = datetime.datetime.now()
+        hours = []
+        view_counts = []
+        current_url_logs = current_url_logs.filter(created_at__year=now.year,
+                                                   created_at__month=now.month,
+                                                   created_at__day=now.day)
+        for hour in [x for x in range(24)]:
+            hours.append(hour)
+            view_counts.append(current_url_logs.filter(created_at__hour=hour).count())
+        return hours, view_counts
+
+    @staticmethod
+    def get_device_data(current_url_logs):
+        """
+        设备统计
+        """
+        device_names = current_url_logs.values_list('device', flat=True)
+        data = []
+        names = list(set(list(device_names)))
+        for device_name in names:
+            device_count = current_url_logs.filter(device=device_name).count()
+            data.append({"name": device_name, "value": device_count})
+        return names, data
+
+    @staticmethod
+    def get_os_data(current_url_logs):
+        """
+        操作系统统计
+        """
+        os_names = current_url_logs.values_list('os_name', flat=True)
+        data = []
+        names = list(set(list(os_names)))
+        for os_name in names:
+            os_count = current_url_logs.filter(os_name=os_name).count()
+            data.append({"name": os_name, "value": os_count})
+        return names, data
