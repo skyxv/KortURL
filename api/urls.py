4850a744a62689b454be125aa1d4fbe8dceaac9b
yandenghong <vincentyim8@163.com>
2019-04-03 17:08:19 +0800

    Created ShortUrlsView

--- api/urls.py
+++ api/urls.py
@@ -13,4 +13,5 @@ app_name = "api"
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('short_urls/', views.ShortUrlsView.as_view(), name='short_urls')
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import CreateLinkMapSerializer
+from apps.utils.get_configs import config
+
+
+class ShortUrlsView(APIView):
+    """
+    批量缩短网址API
+    """
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def serialize(link_maps, invalid_urls):
+        results = []
+        for instance in link_maps:
+            results.append({"long_url": instance.url,
+                            "short_url": config.get_short_url_(instance.code)})
+        return {"results": results, "invalid_urls": invalid_urls}
+
+    def post(self, request):
+        form = CreateLinkMapSerializer(data=request.data, context={'request': request})
+        if form.is_valid(raise_exception=True):
+            link_maps, invalid_urls = form.save()
+            return Response(self.serialize(link_maps, invalid_urls), status=status.HTTP_201_CREATED)
