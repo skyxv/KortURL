834fc5b1c426d565322512824c112cd8e994abcb
yandenghong <vincentyim8@163.com>
2019-04-04 14:58:52 +0800

    Added task for logging of access logs and visits

---
+++ apps/urls/tasks.py
@@ -0,0 +1,18 @@
+"""
+*  Author:   Vincent Yim
+*  FileName: tasks.py
+*  Software: PyCharm
+*  Blog:     https://yandenghong.github.io
+"""
+from __future__ import absolute_import, unicode_literals
+from KortURL.celery_app import app
+from apps.urls.models import LinkMap, AccessLogs
+
+
+@app.task
+def record_log_and_visit_count(request, code):
+    """
+    日志和访问次数记录
+    """
+    LinkMap.objects.add_hit_count(code)
+    AccessLogs.objects.build_log_from_request(request, code)
